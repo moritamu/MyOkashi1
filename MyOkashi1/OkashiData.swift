474c68fa4311d1799b4996b7386cb55e776ed9de
morisum <moris@r.sannet.ne.jp>
2025-03-21 09:12:46 +0900

    dataリクエストまで完成

--- MyOkashi1/OkashiData.swift
+++ MyOkashi1/OkashiData.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 @Observable class OkashiData {
+//    JSONのデータ構造
+    struct ResultJson: Codable {
+        struct Item: Codable {
+            let name: String?
+            let url: URL?
+            let image: URL?
+        }
+        let items: [Item]?
+    }
     func searchOkashi(keyword: String) {
         print("searchOkashiメソッドで受け取った値は \(keyword)")
         Task {
@@ -23,5 +32,16 @@ import SwiftUI
             return
         }
         print(req_url)
+        do {
+//            リクエストURLからダウンロード
+            let (data, _) = try await URLSession.shared.data(from: req_url)
+//            JsonDecoderのインスタンス取得
+            let decoder = JSONDecoder()
+//            受け取ったJSONデータを格納
+            let json = try decoder.decode(ResultJson.self, from: data)
+            print(json)
+        } catch {
+            print("エラーです。\(error)")
+        }
     }
 }
